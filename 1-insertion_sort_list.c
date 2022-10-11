db598546471b364363b6984ec6cd7317d921f8ad
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-10-11 05:33:05 -0700

    Added insertion_sort.c and big o file

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * swap_nodes - swap two nodes in a listint_t doubly-linked list
+ * @head: pointer to the head of the doubly-linked list
+ * @node1: pointer to the first node to swap
+ * @node2: ponter to the second node to swap
+ */
+void swap_nodes(listint_t **head, listint_t **node1, listint_t *node2)
+{
+	(*node1)->next = node2->next;
+	if (node2->next != NULL)
+		node2->next->prev = *node1;
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
+	if ((*node1)->prev != NULL)
+		(*node1)->prev->next = node2;
+	else
+		*head = node2;
+	(*node1)->prev = node2;
+	*node1 = node2->prev;
+}
+
+/**
+ * insertion_sort_list - sort a doubly-linked list using insertion sort
+ * @list: pointer to head of doubly-linked list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *iter, *insert, *tmp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	{
+		tmp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
+		{
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)*list);
+		}
+	}
+}
